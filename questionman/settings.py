6e04003d984da2a4d03d701edfac5780fc05c572
guille0 <guillermovicente@protonmail.com>
2019-05-27 10:34:39 +0200

    added email for password reset

--- questionman/settings.py
+++ questionman/settings.py
@@ -147,4 +147,12 @@ MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+
 django_heroku.settings(locals())
