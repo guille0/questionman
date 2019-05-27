630db357d1d2f8fbee86e91dac15ba077241baa4
guille0 <guillermovicente@protonmail.com>
2019-05-27 10:43:51 +0200

    removed password reset

--- questionman/urls.py
+++ questionman/urls.py
@@ -29,7 +29,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     # path('profile/', users_views.profile, name="profile"),
 
-
     # path('password_reset/',
     #     auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
     #     name="password-reset"),
